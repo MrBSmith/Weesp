2fbcfaf38a95f6d0b84c926fc455526d9ad234c3
Hugo Baba <Hugobarette@gmail.com>
2020-02-17 10:58:20 +0100

    Add working bouncing walls

--- Scenes/Player/Elastic/ElasticMove.gd
+++ Scenes/Player/Elastic/ElasticMove.gd
@@ -10,7 +10,16 @@ func update(_host, _delta):
 		
 		if collision != null:
 			var collider = collision.get_collider()
-			if !collider.is_class("BouncingWall"):
+			# If the collision occurs on a bouncing wall 
+			if collider.is_class("BouncingWall"):
+				# If the collision is a floor or a ceiling
+				if collision.get_position().y <= player_node.get_global_position().y - 4 or collision.get_position().y >= player_node.get_global_position().y + 4:
+					physics_node.velocity.y = -physics_node.velocity.y
+				# if the collision is a wall
+				else:
+					physics_node.velocity.x = -physics_node.velocity.x
+			# If the collision occurs with a normal wall
+			else:
 				return "Glue"
 
 
